79e65ba2d7feef7d25c2df83c35d95b70560947c
Pierre Brito <carlospierre07@gmail.com>
2024-03-22 14:42:23 -0300

    Models de product

---
+++ products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price_setup = models.DecimalField(max_digits=10, decimal_places=2)
+    price_after_sales = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
