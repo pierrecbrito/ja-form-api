23d175522f60b19e06503477d15fda5b0a985b29
Pierre Brito <carlospierre07@gmail.com>
2024-03-22 14:12:09 -0300

    Endpoints de auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from accounts.views.current_user import CurrentUser
 from django.urls import path
 
 urlpatterns = [
-    path('signin', Signin.as_view()),
-    path('signup', Signup.as_view()),
-    path('user', CurrentUser.as_view())
+    path('signin/', Signin.as_view()),
+    path('signup/', Signup.as_view()),
+    path('user/', CurrentUser.as_view())
 ]
